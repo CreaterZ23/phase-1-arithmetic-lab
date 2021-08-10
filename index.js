1720481e1fc9409b7715dff69af836f0b31d8636
Zeon <zeonstewart23@gmail.com>
2021-08-10 00:01:32 -0400

    commit tree f2bcab369c950ce3526ca03a364cdd5ab9e96d0d

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 2,
+    num2 = 31,
+    num3 = 40,
+    num4 = 6
+
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() + 10);
+
+let mod = num3 % num4;
+
+let max = Math.max(20);
